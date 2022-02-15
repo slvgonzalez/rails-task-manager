4bfd8277ca795210ef7602134302d014dbe968e0
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-15 14:23:28 +0000

    very simple styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,12 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
 end
