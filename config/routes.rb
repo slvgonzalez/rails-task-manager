2996c53d32ac17f0aa91c65070ae3e9e2d33540a
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-15 12:31:47 +0000

    CRUD routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See all
+  get '/tasks', to: 'tasks#index'
+  # Create
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'restaurants#create'
+  # Read
+  get '/tasks/:id', to: 'tasks#show'
+  # Update
+  get '/tasks/:id/edit', to: 'restaurants#edit'
+  patch '/restaurants/:id', to: 'restaurants#update'
+  # Delete
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
